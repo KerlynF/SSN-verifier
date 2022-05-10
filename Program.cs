5670d412e3f3143c68d1f63d48da8306b278937a
KerlynF <78883400+KerlynF@users.noreply.github.com>
2022-05-10 12:19:44 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Lab_de_tendencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese los 9 numeros del Social Security Number");
+            List<int> ssn = new List<int>();
+            ConsoleKeyInfo x = new ConsoleKeyInfo();
+            int contador = 0;
+            
+            
+            for( x = Console.ReadKey(true); x.Key != ConsoleKey.Enter; x = Console.ReadKey(true))
+            {
+               if(contador == 9)
+               {
+                   // ValidNumber();
+               }
+               else{
+                Console.Write(x.KeyChar);
+                ssn.Add(Convert.ToInt32(x.KeyChar));
+                ssn
+                contador++;
+                if(contador == 3){
+                    Console.Write("-");
+                }
+                else if(contador == 5){
+                    Console.Write("-");
+                }
+               } 
+            }
+
+
+            
+
+
+        }
+        //funcion para validar el SSN
+        static void ValidNumber(List<int> SSN)
+        {
+            foreach (int number in SSN)
+            {
+                
+            }
+        }
+        
+    }
+}
